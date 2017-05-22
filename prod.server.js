9123716a08a600ea3364775c257872d3584d2289
sanmao233 <273221973@qq.com>
2017-05-22 10:41:10 +0800

    csc

---
+++ prod.server.js
@@ -0,0 +1,54 @@
+/**
+ * Created by 吴利文 on 2017/5/19.
+ */
+var express = require('express');
+var config = require('./config/index');
+var port = process.env.PORT || config.build.port;
+
+var app = express();
+
+var router = express.Router();
+
+router.get('/',function(req,res,next){
+  req.url = '/index.html'
+  next()
+});
+app.use(router);
+
+var appData=require('./data.json')
+//提取数据
+var seller=appData.seller
+var goods=appData.goods
+var ratings=appData.ratings
+//设置路由和api
+var apiRoutes=express.Router()
+apiRoutes.get('/seller',function(req,res){
+  res.json({
+    errno:0,
+    data:seller
+  })
+})
+apiRoutes.get('/goods',function(req,res){
+  res.json({
+    errno:0,
+    data:goods
+  })
+})
+apiRoutes.get('/ratings',function(req,res){
+  res.json({
+    errno:0,
+    data:ratings
+  })
+})
+//启用api
+app.use('/api',apiRoutes)
+
+app.use(express.static('./dist'))
+
+module.exports = app.listen(port,function(err){
+  if(err){
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:'+port+'\n')
+})
